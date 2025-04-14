bdd264683ccbc782a546655516a6548b1bfbc211
coder <coder@coder.com>
2025-04-14 03:00:25 +0300

    add methods to class FuzzyService

--- classification/services/fuzzy.py
+++ classification/services/fuzzy.py
@@ -1,9 +1,12 @@
-from typing import List, Dict, Any
-
-from vsu_task_core.common.fuzzy import FuzzyResult
 from vsu_task_core.common.ner import NamedEntity, NERType
 from vsu_task_core.main.fuzzy import FuzzyRapidMatcher
 
+from classification.constants import (
+    LOCATION_NAMES_EN,
+    LOCATION_NAMES_RU,
+    PERSON_NAMES_EN,
+    PERSON_NAMES_RU,
+)
 from classification.types.fuzzy import (
     DtoFuzzy,
     DtosFuzzy,
@@ -20,17 +23,43 @@ class FuzzyService:
     def __init__(self, threshold: float = 90):
         self.matcher = FuzzyRapidMatcher(threshold=threshold)
 
+    def get_fuzzy_result(
+        self, language: str, entities: ExtractedGroupEntities
+    ) -> FuzzyResultDTO:
+        references = self.get_references(language)
+        matched_persons = self.match_entities(
+            entities["persons"], references["persons"], entity_type="person"
+        )
+        matched_locations = self.match_entities(
+            entities["locations"],
+            references["locations"],
+            entity_type="location",
+        )
+        return {
+            "persons": self.dtos_fuzzy(matched_persons),
+            "locations": self.dtos_fuzzy(matched_locations),
+        }
+
     def match_entities(
         self, entities: NamedEntities, reference: Reference, entity_type: str
     ) -> MatchedEntities:
         named_entity = [
             NamedEntity(
                 NERType.PERSON if entity_type == "person" else NERType.LOCATION,
-                value
+                value,
             )
             for value in reference
         ]
-        return  self.matcher.match(entities, named_entity)
+        return self.matcher.match(entities, named_entity)
+
+    @staticmethod
+    def get_references(language: str) -> References:
+        return {
+            "persons": PERSON_NAMES_RU if language == "ru" else PERSON_NAMES_EN,
+            "locations": (
+                LOCATION_NAMES_RU if language == "ru" else LOCATION_NAMES_EN
+            ),
+        }
 
     @staticmethod
     def dto_fuzzy(matched_entity: MatchedEntity) -> DtoFuzzy:
